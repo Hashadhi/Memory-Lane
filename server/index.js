290dae7c0c6a7ec3ed76780d8c31e47aa7fbb101
Hashadhi <hashadhijayasinghe@gmail.com>
2024-06-03 10:24:37 +1000

    Setup database connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://hashadhij12:hashadhij12@cluster1.2xqq9k5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
